721fc1ce8afe4859356fe83eefa029eb0a0d8844
Steve Yen <steve.yen@gmail.com>
2020-09-07 12:17:17 -0700

    SessionAssignContainer() helper func

--- cmd/play-server/run_session.go
+++ cmd/play-server/run_session.go
@@ -7,43 +7,12 @@ import (
 
 func RunRequestSession(session *Session, req RunRequest,
 	readyCh chan int, containerWaitDuration time.Duration,
-	restartCh chan<- Restart) ([]byte, error) {
-	if session != nil && session.ContainerId < 0 {
-		containerId, err := WaitForReadyContainer(
-			req.ctx, readyCh, containerWaitDuration)
-		if err != nil {
-			return nil, err
-		}
-
-		defer func() {
-			if containerId >= 0 {
-				restartCh <- Restart{
-					ContainerId: containerId,
-					ReadyCh:     readyCh,
-				}
-			}
-		}()
-
-		err = AddRBACUser(req, containerId,
-			session.CBUser, session.CBPswd, "admin")
-		if err != nil {
-			return nil, err
-		}
-
-		session = sessions.SessionAccess(session.SessionId,
-			func(session *Session) *Session {
-				session.ContainerId = containerId
-				session.RestartCh = restartCh
-				session.ReadyCh = readyCh
-				session.TouchedAt = time.Now().Unix()
-
-				// Session owns the containerId.
-				containerId = -1
-
-				rv := *session // Copy.
-
-				return &rv
-			})
+	restartCh chan<- Restart) (out []byte, err error) {
+	session, err = SessionAssignContainer(session, req,
+		readyCh, containerWaitDuration, restartCh)
+	if err != nil {
+		return nil, fmt.Errorf("RunRequestSession,"+
+			" could not assign container, err: %v", err)
 	}
 
 	if session == nil {
@@ -51,4 +20,58 @@ func RunRequestSession(session *Session, req RunRequest,
 	}
 
 	return RunRequestInContainer(req, session.ContainerId)
+}
+
+// ------------------------------------------------
+
+func SessionAssignContainer(session *Session, req RunRequest,
+	readyCh chan int, containerWaitDuration time.Duration,
+	restartCh chan<- Restart) (*Session, error) {
+	if session == nil {
+		return nil, fmt.Errorf("SessionAssignContainer, no session")
+	}
+
+	if session.ContainerId >= 0 {
+		return session, nil
+	}
+
+	containerId, err := WaitForReadyContainer(
+		req.ctx, readyCh, containerWaitDuration)
+	if err != nil {
+		return session, err
+	}
+
+	defer func() {
+		if containerId >= 0 {
+			restartCh <- Restart{
+				ContainerId: containerId,
+				ReadyCh:     readyCh,
+			}
+		}
+	}()
+
+	err = AddRBACUser(req, containerId,
+		session.CBUser, session.CBPswd, "admin")
+	if err != nil {
+		return nil, err
+	}
+
+	session = sessions.SessionAccess(session.SessionId,
+		func(session *Session) *Session {
+			if session.ContainerId < 0 {
+				session.ContainerId = containerId
+				session.RestartCh = restartCh
+				session.ReadyCh = readyCh
+				session.TouchedAt = time.Now().Unix()
+
+				// Session owns the containerId.
+				containerId = -1
+			}
+
+			rv := *session // Copy.
+
+			return &rv
+		})
+
+	return session, nil
 }
