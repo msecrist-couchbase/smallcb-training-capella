dd0002295cdbff5c9f149727e326d8ed8f3df538
Steve Yen <steve.yen@gmail.com>
2020-09-11 10:40:41 -0700

    simplified go example to working hello world for now

--- cmd/play-server/run-go.sh
+++ cmd/play-server/run-go.sh
@@ -4,6 +4,10 @@ export GOPATH=/go
 
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
+mkdir -p $(dirname ${1})/.gocache
+
+export GOCACHE=$(dirname ${1})/.gocache
+
 cd $(dirname ${1})
 
 go run code.go
