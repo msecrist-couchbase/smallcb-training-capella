bd73309b3d7dd141565f6a9a57912ad8796ef5ad
Steve Yen <steve.yen@gmail.com>
2020-09-15 12:38:25 -0700

    gen-examples started

---
+++ cmd/gen-examples/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	m, err := ReadFiles("../sdk-examples/go", map[string]bool{
+		"go": true,
+	}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for suffix, m2 := range m {
+		for name, code := range m2 {
+			log.Printf("suffix: %s, name: %s", suffix, name)
+
+			d := map[string]string{
+				"chapter": "go",
+				"page":    "",
+				"title":   suffix + ": " + strings.ReplaceAll(name, "-", " "),
+				"lang":    "go",
+				"code":    code,
+			}
+
+			b, err := yaml.Marshal(d)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = ioutil.WriteFile(fmt.Sprintf(
+				"./cmd/play-server/static/examples/gen_%s_%s.yaml",
+				suffix, name), b, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func ReadFiles(dir string, suffixes map[string]bool,
+	// Keyed by suffix (i.e., "go"), then by basename, and value is contents.
+	rv map[string]map[string]string) (
+	map[string]map[string]string, error) {
+	if rv == nil {
+		rv = map[string]map[string]string{}
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("ioutil.ReadDir, dir: %s, err: %v", dir, err)
+	}
+
+	for _, f := range files {
+		name := f.Name()
+
+		parts := strings.Split(name, ".")
+
+		suffix := parts[len(parts)-1]
+
+		if suffixes[suffix] {
+			path := dir + "/" + name
+
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				return nil, fmt.Errorf("ioutil.ReadFile, path: %s, err: %v", path, err)
+			}
+
+			m2 := rv[suffix]
+			if m2 == nil {
+				m2 = map[string]string{}
+				rv[suffix] = m2
+			}
+
+			m2[name[0:len(name)-len(suffix)-1]] = string(b)
+		}
+	}
+
+	return rv, nil
+}
