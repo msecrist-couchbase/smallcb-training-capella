1926b7acba49c4fe2f50c6147c30b321df5ff6cc
Eric Bishard <e.bishard@gmail.com>
2020-10-14 18:24:29 -0700

    Add chevron to session-info expansion button Add JS to swap classname on chevron Add Style for CSS only chevron icon

--- cmd/play-server/static/scripts/infoPanel.js
+++ cmd/play-server/static/scripts/infoPanel.js
@@ -3,14 +3,22 @@ function setElemDisplay(elemId, displayValue) {
   elem.style.display = displayValue
 }
 
+function setChevronDirection(elemId, direction) {
+  let elem = document.getElementById(elemId);
+  elem.className = 'chevron'
+  elem.className += ` ${direction}`
+}
+
 function toggleInfoPanel() {
   const ls = localStorage.getItem('infoPanel')
   if (ls === 'show') {
     localStorage.setItem('infoPanel', 'hide')
     setElemDisplay('infoPanel', 'none')
+    setChevronDirection('toggleIcon','down')
   } else if (ls === 'hide') {
     localStorage.setItem('infoPanel', 'show')
     setElemDisplay('infoPanel', 'block')
+    setChevronDirection('toggleIcon','up')
   }
 }
 
@@ -18,8 +26,10 @@ function infoPanelInitPageLoad() {
   const ls = localStorage.getItem('infoPanel')
   if(ls == 'hide') {
     setElemDisplay('infoPanel', 'none')
+    setChevronDirection('toggleIcon','down')
   } else if(ls == 'show') {
     setElemDisplay('infoPanel', 'block')
+    setChevronDirection('toggleIcon','up')
   } else if(ls === null) {
     localStorage.setItem('infoPanel', 'show')
   }
