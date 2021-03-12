3fc08b2358b1f98983718d4cc5185b5722a491ea
Steve Yen <steve.yen@gmail.com>
2021-03-12 14:53:34 -0800

    sort examples by optional chapter-order and page-order

--- cmd/play-server/main_template.go
+++ cmd/play-server/main_template.go
@@ -298,7 +298,9 @@ func ReadExamples(dir string) (
 
 		iex, jex := examples[iname], examples[jname]
 
-		for _, k := range []string{"chapter", "page", "title-order", "title"} {
+		for _, k := range []string{
+			"chapter-order", "chapter", "page-order", "page", "title-order", "title",
+		} {
 			iv := MapGetString(iex, k)
 			jv := MapGetString(jex, k)
 
