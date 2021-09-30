798ea02ab4d5f088225ad6742e5fe4f090df37e9
Nithish Raghunandanan <nithish.raghunandanan@couchbase.com>
2021-09-30 17:55:25 +0200

    Fix review comments

--- cmd/play-server/run-dotnet.sh
+++ cmd/play-server/run-dotnet.sh
@@ -3,7 +3,7 @@
 cd $(dirname ${1})
 
 # csproj file, among other things, determines which packages to pull from nuget
-echo '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp3.1</TargetFramework></PropertyGroup><ItemGroup><PackageReference Include="CouchbaseNetClient" Version="3.2.2" /></ItemGroup></Project>' > project.csproj
+echo '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp3.1</TargetFramework></PropertyGroup><ItemGroup><PackageReference Include="CouchbaseNetClient" Version="3.1.3" /></ItemGroup></Project>' > project.csproj
 
 # this file extension shouldn't be 'dotnet' in the first place
 # Simple hack to workaround for JIRA:NCBC-2945
--- cmd/play-server/run-scala-build.sbt
+++ cmd/play-server/run-scala-build.sbt
@@ -4,5 +4,5 @@ ThisBuild / organization := "com.couchbase"
 lazy val example = (project in file("."))
   .settings(
     name := "Example",
-    libraryDependencies += "com.couchbase.client" %% "scala-client" % "1.2.1",
+    libraryDependencies += "com.couchbase.client" %% "scala-client" % "1.1.2",
   )
