c4acd9ec6d5368035244c3ce86001f8578618b28
Jagadesh Munta <jagadesh.munta@couchbase.com>
2022-01-10 19:57:40 -0800

    DEVADV-1671: restricting packages for python examples execution

--- cmd/play-server/run-py.sh
+++ cmd/play-server/run-py.sh
@@ -2,4 +2,9 @@
 
 cd $(dirname ${1})
 
+IS_SEC_RESTRICT=`egrep -e "import[ .]+[']?os|platform|pkg_resources|socket|urlib|server|internal[']?[ .]*" code.py`
+if [ "${IS_SEC_RESTRICT}" != "" ]; then
+    echo "Security restriction: some packages are not allowed."
+    exit 1
+fi
 python3 code.py
