2f720522d22fa89d3ec48eb6fabf1334f386c636
Jagadesh Munta <jagadesh.munta@couchbase.com>
2022-01-11 12:05:52 -0800

    fixed the urllib

--- cmd/play-server/run-py.sh
+++ cmd/play-server/run-py.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname ${1})
 
-IS_SEC_RESTRICT=`egrep -e "import[ .]+[']?os|platform|pkg_resources|socket|urlib|base64|server|internal[']?[ .]*" code.py`
+IS_SEC_RESTRICT=`egrep -e "import[ .]+[']?os|platform|pkg_resources|socket|urllib|base64|server|internal[']?[ .]*" code.py`
 if [ "${IS_SEC_RESTRICT}" != "" ]; then
     echo "Security restriction: some packages are not allowed."
     exit 1
